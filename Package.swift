0fe37dbed51d4774b2e8a6e175085d41c87d2904
DexJoke <50515038+DexJoke@users.noreply.github.com>
2022-07-21 14:39:44 +0700

    downgrate to ios11

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-fcsdk-ios",
-    platforms: [.iOS(.v13), .macOS(.v11)],
+    platforms: [.iOS(.v11), .macOS(.v11)],
     products: [
         .library(
             name: "FCSDKiOS",
